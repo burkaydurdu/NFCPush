56e9b38e0634d5f44c40f13e1ce2b479731a479e
burkay <burkaydurdu@outlook.com>
2019-07-18 01:27:04 +0300

    Added wifi client control flag. and edited code base.

--- pushnot.ino
+++ pushnot.ino
@@ -1,7 +1,5 @@
-
-#define SS_PIN D4
-#define RST_PIN D2
-
+#define SS_PIN D4 // RFID SDA PIN
+#define RST_PIN D2 // RFID RST PIN
 
 #include <WiFiClientSecure.h>   // Include the HTTPS library
 #include <ESP8266WiFi.h>        // Include the Wi-Fi library
@@ -10,17 +8,15 @@
 #include <MFRC522.h>
 
 
-
 ESP8266WiFiMulti mWifi;
-
+WiFiClientSecure client;
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-
-const char* ssid = "";
-const char* pass = "";
 
 // Push Notification
 const char* host = "api.pushbullet.com";
-const char* PushBulletAPIKEY = "o.IQnj3KHununjIAevHjgtu1gk8LWm9GTm";
+const char* PushBulletAPIKEY = "XXX";
+
+bool isPush = false;
 
 const char* fingerprint = "2C BC 06 10 0A E0 6E B0 9E 60 E5 96 BA 72 C5 63 93 23 54 B3";
 const int httpsPort = 443;
@@ -28,15 +24,15 @@ const int httpsPort = 443;
 void setup() {
 	Serial.begin(9600);
 	setupWifi();
-	delay(500);
-	setupWebClient();
 	setupMfrc();
+	isPush = setupWebClient();
 }
 
 void loop() {
 	if ( ! mfrc522.PICC_IsNewCardPresent()) return;
 	if ( ! mfrc522.PICC_ReadCardSerial()) return;
 	Serial.println(readRfid().substring(1));
+	if (isPush) pushNotification();
 	delay(300);
 }
 
@@ -59,8 +55,9 @@ void setupMfrc() {
 }
 
 void setupWifi() {
-	mWifi.addAP("VODAFONE_D905", "Hal1Bu2Ev");
-	mWifi.addAP("Pisano DEV-2G", "p15@n0wifi");
+	mWifi.addAP("xxx", "xxx");
+	mWifi.addAP("xxx", "xxx");
+	mWifi.addAP("xxx", "xxx");
 	
 	Serial.println("Connecting ...");
 	// Wait for the Wi-Fi to connect
@@ -77,33 +74,51 @@ void setupWifi() {
 	Serial.println('\n');
 }
 
-void setupWebClient() {
-	// Use WiFiClientSecure class to create TLS connection
-		WiFiClientSecure client;
-		Serial.print("connecting to ");
-		Serial.println(host);
-		if (!client.connect(host, httpsPort)) {
-			Serial.println("connection failed");
-			return;
-		}
-
-		if (client.verify(fingerprint, host)) {
-			Serial.println("certificate matches");
-		} else {
-	 		Serial.println("certificate doesn't match");
-		}
-		String url = "/v2/pushes";
-		String messagebody = "{\"type\": \"note\", \"title\": \"ESP8266\", \"body\": \"Hello World!\"}\r\n";
-		Serial.print("requesting URL: ");
-		Serial.println(url);
-		
-		client.print(String("POST ") + url + " HTTP/1.1\r\n" + 
-												"Host: " + host + "\r\n" +
-												"Authorization: Bearer " + PushBulletAPIKEY + "\r\n" +
-												"Content-Type: application/json\r\n" +
-											  "Content-Length: " + 
-												String(messagebody.length()) + "\r\n\r\n");
+bool setupWebClient() {
+	// Use WiFiClientSecure class to create TLS connection	
+	Serial.print("connecting to ");
+	Serial.println(host);
 	
-		client.print(messagebody);
-		Serial.println("request sent");
+	client.setInsecure();
+
+	if (!client.connect(host, httpsPort)) {
+		Serial.println("connection failed");
+		return false;
+	}
+
+	if (client.verify(fingerprint, host)) {
+		Serial.println("certificate matches");
+	} else {
+	 	Serial.println("certificate doesn't match");
+		return false;
+	}
+
+	return true;
+}
+
+void pushNotification() {
+	String url = "/v2/pushes";
+	String messagebody = "{\"type\": \"note\", \"title\": \"ESP8266\", \"body\": \"Hello World!\"}\r\n";
+
+	String messagebody_link = "{\"type\": \"link\", \"title\": \"Pis\", \"body\": \"Hii^^!\",\"device_iden\": \"ujCdJBKOTEOsjyaFXj1GXA\", \"url\": \"https://www.google.com\"}\r\n";
+
+	Serial.print("requesting URL: ");
+	Serial.println(url);
+		
+	client.print(
+		String("POST ") + url + " HTTP/1.1\r\n" + 
+		"Host: " + host + "\r\n" +
+		"Authorization: Bearer " + PushBulletAPIKEY + "\r\n" +
+		"Content-Type: application/json\r\n" +
+		"Content-Length: " + 
+		String(messagebody_link.length()) + "\r\n\r\n");
+	
+	client.print(messagebody_link);
+	Serial.println("request sent");
+
+	while (client.available() == 0);
+	while (client.available()) {
+		String line = client.readStringUntil('\n');
+		Serial.println(line);
+	}
 }
